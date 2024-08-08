8589acb2ae470f447dc874fcb4f33ab6b9d2abcd
skyclaw234 <112114475+skyclaw234@users.noreply.github.com>
2024-08-08 13:55:59 -0400

    kk

--- php/form-process.php
+++ php/form-process.php
@@ -1,56 +1,25 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['p_name'];
+    $email = $_POST['p_email'];
+    $subject = $_POST['p_subject'];
+    $message = $_POST['p_message'];
 
-$errorMSG = "";
+    $to = 'brantsafrit@gmail.com';
+    $subject = "Contact Form Submission: " . $subject;
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
 
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
-} else {
-    $name = $_POST["name"];
-}
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-// EMAIL
-if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
-} else {
-    $email = $_POST["email"];
-}
-
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
-
-
-$EmailTo = "emailaddress@test.com";
-$Subject = "New Message Received";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
     } else {
-        echo $errorMSG;
+        echo "Failed to send message.";
     }
+} else {
+    echo "Invalid request method.";
 }
-
 ?>
